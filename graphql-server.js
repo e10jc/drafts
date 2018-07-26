8523fd5cbaf4fd832c990c343f9e852d07754b08
Ethan Jon <e10jc@users.noreply.github.com>
2018-07-26 12:12:45 -0700

    sign up and sign in mutation

--- graphql-server.js
+++ graphql-server.js
@@ -1,6 +1,8 @@
 const {ApolloServer, gql} = require('apollo-server-express')
+const {compare, hash} = require('bcrypt')
 
 const Draft = require('./models/draft')
+const User = require('./models/user')
 
 const typeDefs = gql`
   type Draft {
@@ -19,12 +21,31 @@ const typeDefs = gql`
     email: String
   }
 
+  type Mutation {
+    createUser(email: String!, password: String!): User
+    loginUser(email: String!, password: String!): User
+  }
+
   type Query {
     drafts: [Draft]
   }
 `
 
 const resolvers = {
+  Mutation: {
+    createUser: async (obj, {email, password}) => {
+      const hashedPassword = await hash(password, 10)
+      const user = await User.query().insert({email, password: hashedPassword})
+      return user
+    },
+
+    loginUser: async (obj, {email, password}) => {
+      const user = await User.query().where({email}).first()
+      if (user && await compare(password, user.password)) {
+        return user
+      }
+    },
+  },
   Query: {
     drafts: async (obj, args, context, info) => Draft.query().eager('[prompt, user]')
   }
